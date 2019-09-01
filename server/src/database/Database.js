3b7b27c498548076770232cf48aa2d99d4e601b8
Dheyson Alves <dheyson.lucas@hotmail.com>
2019-09-01 06:22:08 -0300

    Adiciona promises no arquivo database

--- server/src/database/Database.js
+++ server/src/database/Database.js
@@ -1,13 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 /*
 mongoose.connect('mongodb+srv://admin:1234@cluster0-wtwy9.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true });
 */
 
-mongoose.connect('mongodb://192.168.0.99/resumidos', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://192.168.0.99/resumidos", {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    mongoose.connection.once("open", () =>
+      console.log("Successful Connection to MongoDB!")
+    );
+  })
+  .catch(err => {
+    mongoose.connection.on("error" + err);
+  });
 
-mongoose.connection.on('error', console.error);
-mongoose.connection.once('open', () => console.log('Successful Connection to MongoDB!'));
+mongoose.Promise = global.Promise;
 
-module.exports = { mongoose }
+module.exports = { mongoose } 
