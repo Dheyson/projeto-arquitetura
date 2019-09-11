8718d24d94e44abcb9158433851e8a946ae479ed
OrlandoNetto <orlandonetto11@gmail.com>
2019-09-11 00:12:50 -0300

    Servidor modificado ++

--- server/src/api/Server.js
+++ server/src/api/Server.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken');
-const { verifyToken } = require('./verifyToken')
-//const userController = require('../controllers/UserController.js')
+const tokenService = require('../services/tokenService')
+const userController = require('../controllers/UserController.js')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-
-/////////
-let userTemp = {}
-/////////
+app.use(bodyParser.json())
 
 const port = 3000
 app.listen(port, () => {
@@ -17,68 +13,14 @@ app.listen(port, () => {
 })
 
 app.set('json spaces', 2)
-app.use(bodyParser.json())
 app.use(async (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "*")
     next()
 })
 
-app.post('/login', (req, res) => {
-    console.log('Server.login');
-    console.log(req.body);
+app.post('/login', userController.login)
 
-    if(userTemp.username !== req.body.username) {
-        return res.status(400).json({
-            message: 'Invalid Username',
-            error: true
-        })
-    }
-    if(userTemp.password !== req.body.password) {
-        return res.status(400).json({
-            message: 'Invalid Password',
-            error: true
-        })
-    }
+app.post('/register', userController.register)
 
-    const token = jwt.sign({ id: userTemp.id },
-        '123',
-        { expiresIn: 86400 });
-
-    return res.status(200).json({
-        token,
-        expiresIn: 86400
-    })
-})
-
-app.post('/register', (req, res) => {
-    console.log('Server.register')
-    console.log(req.body)
-
-    // implementar codigo de save...
-    userTemp = {
-        id: 1,
-        name: req.body.name,
-        username: req.body.username,
-        password: req.body.password
-    }
-
-    res.json({
-        message: 'Register Successfull',
-        user: userTemp
-    })
-})
-
-app.get('/load-session', verifyToken, (req, res) => {
-    console.log('Server.load-session')
-
-    res.json({
-        message: 'load-session sucessfull',
-        user: userTemp
-    })
-})
-
-//app.post('/login', userController.login)
-//app.post('/register', userController.register)
-//app.get('/load-session', userController.loadSession)
-//app.use('/', userController.loadSession)
+app.get('/load-session', tokenService.verifyToken, userController.loadSession)
--- server/src/database/Database.js
+++ server/src/database/Database.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb+srv://admin:1234@cluster0-wtwy9.mongodb.net/test?retry
     { useNewUrlParser: true });
 */
 
-mongoose.connect('mongodb://192.168.0.99/resumidos', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/resumidos', { useNewUrlParser: true })
 
 mongoose.connection.on('error', console.error);
 mongoose.connection.once('open', () => console.log('Successful Connection to MongoDB!'));
