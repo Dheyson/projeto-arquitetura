960c99fbb7805418f066b1dc98d33b869617c7ba
Dheyson Alves <dheyson.lucas@hotmail.com>
2019-09-13 00:06:32 -0300

    Modifica database

--- server/src/database/Database.js
+++ server/src/database/Database.js
@@ -1,27 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 /*
 mongoose.connect('mongodb+srv://admin:1234@cluster0-wtwy9.mongodb.net/test?retryWrites=true&w=majority',
     { useNewUrlParser: true });
 */
 
+mongoose.connect('mongodb://192.168.0.99/resumidos', { useNewUrlParser: true })
 
-mongoose
-  .connect("mongodb://192.168.0.99/resumidos", {
-    useNewUrlParser: true,
-    useFindAndModify: true,
-    useCreateIndex: true
-  })
-  .then(() => {
-    mongoose.connection.once("open", () =>
-      console.log("Successful Connection to MongoDB!")
-    );
-  })
-  .catch(err => {
-    mongoose.connection.on("error" + err);
-  });
-
-
-mongoose.Promise = global.Promise;
+mongoose.connection.on('error', console.error);
+mongoose.connection.once('open', () => console.log('Successful Connection to MongoDB!'));
 
 module.exports = { mongoose } 
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,28 +1,15 @@
-const db = require('../database/Database')
+const db = require('../repositories/Database')
 
 const UserSchema = new db.mongoose.Schema({
-    name: {
-        type: String,
-        require: true
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true,
-        select: false, 
-        // O select Especifica o comportamento padrão de seleção de caminho. Em outras palavras, você pode especificar se esse caminho deve ser incluído ou excluído dos resultados da consulta por padrão.
-    },
+
     createdAt: {
         type: Date,
         default: Date.now
     },
+    username: String,
+    password: String,
+    name: String
+
 })
 
-
-
-module.exports = db.mongoose.model('User', UserSchema)
+module.exports = db.mongoose.model('User', UserSchema) 
